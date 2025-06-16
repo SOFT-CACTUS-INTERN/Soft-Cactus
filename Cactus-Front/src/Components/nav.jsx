d8e250a468a208ebe8dd6d5ab9e8a6d96e57908d
Movved <mohabellatrach@gmail.com>
2025-06-16 15:11:24 +0100

    changes

---
+++ Cactus-Front/src/Components/nav.jsx
@@ -0,0 +1,12 @@
+import '../Styles/nav.css'
+
+function Nav() {
+
+  return (    
+    <>
+    <h1>nav</h1>
+    </>
+  )
+}
+
+export default Nav
---
+++ Cactus-Front/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        Lpurple: '#9D04D3',   
+        Mpurple: '#5911AA', // purple-600
+        Dpurple: '#3C096C',
+        Yellow: '#FEBB08', 
+
+      },
+      fontFamily:{
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
