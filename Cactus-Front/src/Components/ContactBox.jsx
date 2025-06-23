488fcd672ed35391a3f7100412c7fa43b39740e4
Movved <mohabellatrach@gmail.com>
2025-06-23 15:58:38 +0100

    responsive contact | need the gallery

--- Cactus-Front/src/Components/ContactBox.jsx
+++ Cactus-Front/src/Components/ContactBox.jsx
@@ -24,11 +24,11 @@ const ContactBox = ()=>{
                             <input type="text" name="fullName" id="fullName" size="18" placeholder=' Nom et prénom ' />
                             <label htmlFor="societyName">et je représente la société </label>
                             <input type="text" name="societyName" id="societyName" size="14" placeholder=' Nom de la société '/>
-                                <p>je vous contacte pour voir avec vous la possibilité de collaborer sur</p>
-                            <label htmlFor="projectName"> un projet de</label>
+                                je vous contacte pour voir avec vous la possibilité de collaborer sur
+                            <label htmlFor="projectName"> un projet de </label>
                             <input type="text" name="projectName" id="projectName" size="12" placeholder='Nom du projet' />
                             <label htmlFor="email">vous pouvez me contacter sur </label>
-                            <input type="email" name="email" id="email"  placeholder='Adresse email'/> ou 
+                            <input type="email" name="email" id="email"  placeholder='Adresse email'/>  ou  &nbsp; 
                             <input type="tel" name="number" id="number" size="16" placeholder='Numéro de téléphone'/>
                         <br />
                         <div className={contactBox.bouton}>
@@ -42,8 +42,10 @@ const ContactBox = ()=>{
                             <p className={contactBox.contactItem} style={{ paddingRight: '5em' }}><img src={phone} alt="num : " /><a href="tel:+212 648 927 820">+212 648 927 820</a></p>
                             <p className={contactBox.contactItem}><img src={email} alt="email : " /><a href="mailto:contact@softcactus.ma">contact@softcactus.ma</a></p>
                         </div>           
-                            <p className={contactBox.contactItem}><img src={localisation} alt="Local Oujda : " />6 éme Etg, Immeuble EL MOHANDISSINE, Rue Ibn Rochd، Oujda</p>
-                            <p className={contactBox.contactItem}><img src={localisation} alt="Local Casa : " />Etage 1, Angle Mustapha El Màani, Centre Riad, 61, N°39 Av. Lalla Yacout, Casablanca </p>
+                        <div className={contactBox.localContainer}>
+                            <p className={contactBox.localItem}><img src={localisation} alt="Local Oujda : "  />6 éme Etg, Immeuble EL MOHANDISSINE, Rue Ibn Rochd، Oujda</p>
+                            <p className={contactBox.localItem}><img src={localisation} alt="Local Casa : " />Etage 1, Angle Mustapha El Màani, Centre Riad, 61, N°39 Av. Lalla Yacout, Casablanca </p>
+                        </div>
                     </div>
                 
 
--- Cactus-Front/src/Components/EmploisCard.jsx
+++ Cactus-Front/src/Components/EmploisCard.jsx
@@ -6,7 +6,7 @@ const EmploisCard = ()=>{
         <div className={style.Card}>
             <div className={style.content}>
                 <div className={style.service}>
-                    <h1>SERVICE COMMERCIAL<span className={style.yellowDot}>.</span></h1>
+                    <p className={style.head}>SERVICE COMMERCIAL<span className={style.yellowDot}>.</span></p>
                     <p className={style.job}>l'ingénieur commercial</p>
                     <p><a href="tel:+212 664 363 040">+212 664 363 040</a></p>
                     <p><a href="mailto:servicecommercial@softcactus.ma">servicecommercial@softcactus.ma</a></p>
@@ -14,7 +14,7 @@ const EmploisCard = ()=>{
                 <div className={style.vline} />
                 <div className={style.hline} />
                 <div className={style.emplois}>
-                    <h1>LES EMPLOIS<span className={style.yellowDot}>.</span></h1>
+                    <p className={style.head}>LES EMPLOIS<span className={style.yellowDot}>.</span></p>
                     <p>Emplois disponible</p>
                     <p><a href="https://demo.softcactus.ma/career">Jobs@softcactus.ma</a></p>                </div>
 
