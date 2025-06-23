01e0ffad2e57c2b5ab21c4a06c0d541fc59654bf
Movved <mohabellatrach@gmail.com>
2025-06-23 10:30:43 +0100

    the arrow works

--- Cactus-Front/src/Components/ArrowUp.jsx
+++ Cactus-Front/src/Components/ArrowUp.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import arrow from '/src/assets/icons/scroll/arrow.svg'
 import style from '/src/Styles/arrowUp.module.css'
+import { motion } from 'framer-motion';
 
 const ArrowUp =()=>{
+  const [isVisible,setIsVisible]=useState(false)
+ useEffect(()=>{
+        const handleScroll=()=>{
+            if(window.scrollY>300)
+                setIsVisible(true)
+            else
+                setIsVisible(false)
+        };
+        window.addEventListener('scroll',handleScroll);
+        return ()=>{
+            window.removeEventListener('scroll',handleScroll);
+        };
+    },[]);
 
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  if(!isVisible) return;
     return(
-        <div className={style.arrow}>
-                <img src={arrow} alt="go up" />
-        </div>
+        <motion.div 
+            animate={{ y: [0, -20, 0], }}
+            transition= {{
+                duration: 2,
+                 repeat : Infinity,
+                ease: 'easeInOut',}}
+            className={style.arrow}
+            onClick={scrollToTop}
+                     
+                    
+
+        >
+        <img src={arrow} className={style.arrow} alt="go up" />
+        </motion.div>
     )
 };
 export default ArrowUp;
