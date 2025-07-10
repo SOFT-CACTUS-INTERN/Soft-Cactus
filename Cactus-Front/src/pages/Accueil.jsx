8436e089d0b30d9d58fe8bf6a337459b07460f9a
Movved <mohabellatrach@gmail.com>
2025-07-10 17:42:54 +0100

    Nopage 404 page

--- Cactus-Front/src/pages/Accueil.jsx
+++ Cactus-Front/src/pages/Accueil.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import home from '/src/Styles/accueil.module.css'
 import Bar from '../Components/AnimeBars.jsx'
-import LaTeam from '../Components/LaTeam.jsx'
+
 
 const Accueil = () => {
   
  return (
   <div className={home.hh}>
     <Bar />
-    <LaTeam />
+    
   </div>
 )
 }
--- Cactus-Front/src/pages/Nopage.jsx
+++ Cactus-Front/src/pages/Nopage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Nopage = () => {
  return (
-    <p>ERROR 404!</p>
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! This page doesn't exist.</p>
+      <a href="/">Return Home</a>
+    </div>
  )
 }
 
