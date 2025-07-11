fd350852a181a4401776a3aec78b9961c7435aa1
Movved <mohabellatrach@gmail.com>
2025-07-11 14:52:01 +0100

    clean up

--- emailSending/mailAPI/resources/views/emails/contact.blade.php
+++ emailSending/mailAPI/resources/views/emails/contact.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <p>Bonjour, je m'appelle <strong>{{ $data['fullName'] }}</strong>  et je représente la société <strong>{{ $data['societyName'] }}</strong> 
-    je vous contacte pour voir avec vous la possibilité de collaborer sur un projet de <b> {{ $data['projectName'] }}</b>
-     vous pouvez me contacter sur <b>{{ $data['email'] }}</b> ou <b>{{ $data['number'] }}</b>  </p>
+        je vous contacte pour voir avec vous la possibilité de collaborer sur un projet de <b> {{ $data['projectName'] }}</b>
+        vous pouvez me contacter sur <b>{{ $data['email'] }}</b> ou <b>{{ $data['number'] }}</b>  </p>
 </body>
 </html>
